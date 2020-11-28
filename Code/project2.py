7a64dfcc14274c6c0a8aa2e644721e4b635a6fb8
amodkk <randomambipom@gmail.com>
2020-11-28 17:38:31 -0600

    perfected otuput, matching functionality foundation in place

--- Code/project2.py
+++ Code/project2.py
@@ -12,67 +12,55 @@
 #---------------------------------------------------------------------------------------------------------------------------------
 from pathlib import Path
 
-#CONSTANTS ----------------------------------------------------------
-#Get paths right  IDE has different working directory for some reason
-CURRENTDIR = Path(__file__).parent #dunder/special variable __file__ 
-DISKIMG = "Project2Updated.dd"
-OUTPUTFILE = "AnalysisOutput_Hex.txt"
-DISKIMG_PATH = CURRENTDIR.joinpath(DISKIMG)
-OUTPUT_PATH = CURRENTDIR.joinpath(OUTPUTFILE)
-DISKIMG_SIZE = Path(DISKIMG_PATH).stat().st_size #size of the disk image, in bytes 
-
-#SIGNATURES
+#"CONSTANTS" ----------------------------------------------------------
+#Signatures
 PDF_SIG = "25-50-44-46"
 
-#debug
-#print(DISKIMG_PATH)
-diskImg = open(DISKIMG_PATH, "rb")
-#output_file is really for manual debugging, as we can't scroll all the way up on this huge output 
-outputFile = open(OUTPUT_PATH, "w")
+#Paths
+CURRENTDIR = Path(__file__).parent #dunder/special variable __file__ 
+DISKIMG_FILENAME = "Project2Updated.dd"
+OUTPUT_FILENAME = "AnalysisOutput.txt"
+DISKIMG_PATH = CURRENTDIR.joinpath(DISKIMG_FILENAME)
+OUTPUT_PATH = CURRENTDIR.joinpath(OUTPUT_FILENAME)
+DISKIMG_SIZE = Path(DISKIMG_PATH).stat().st_size #size of the disk image, in bytes 
+#Files 
+DISKIMG = open(DISKIMG_PATH, "rb")
+OUTPUT_FILE = open(OUTPUT_PATH, "w") #for manual debugging
+
 
 offset = 0 #current byte offset location in disk
 progress = 0 #progress made through disk analysis (in percent)
 bufsize = 4096 #to load larger amount of bytes at once into t he program vs constsantly having to read from OS. speeds up program
-buf = diskImg.readlines(bufsize)
+
 
 #update disk analysis every progress every 5%
 def updateProgress(offset):
         global progress
         new_prog = offset/DISKIMG_SIZE * 100
         if(new_prog >= progress + 5): 
-                progress = new_prog
-                print("Progress: " + str(progress) + " %")
+                progress = int(new_prog)
+                print("Progress: " + str(progress) + " %") 
 
-#writes diskImg information to file in hex format
-def writeToFile(offset, bytes_len, outputFile): 
-        offset_str = "Current offset: " + str(offset)
-        bytes_len = "Bytes (line length): " + str(line.__len__())
-        output = offset_str + "\n" + bytes_len + "\n" + line.hex("-") + "\n" #now its all as hex, as in active desk editor
-        #print(output) #debugging purposes
-        outputFile.write(output + "\n")
+#match signatures-- very premature version
+def matchSignatures(offset, data): 
+        match = str(data).find(PDF_SIG)
+        if(match != -1): 
+                print("Potential PDF found at offset " + str(match + offset))
 
-#goes through the lines of the diskimage and prints it in hex format to output file
-while buf: 
-        for line in buf: #line is of type 'bytes'
-                writeToFile(offset, line.__len__(), outputFile)
+#this loop reads the diskimage and prints it in hex format to output file
+buf = DISKIMG.read(bufsize)
+while buf: #continues looping as long as 'buf' variable is populated
+        sliceStart = 0
+        SLICE_SIZE = 16
+        #debug
+        buffLength = buf.__len__() 
+        while (sliceStart < buffLength): 
+                line = buf[sliceStart:sliceStart + SLICE_SIZE].hex('-') #16-byte line of the diskimage data in hex format
+                matchSignatures(offset, line)
+                #match signatures here
+                OUTPUT_FILE.write("Offset " + str(offset) + " | " + line  + "\n")
+                offset += SLICE_SIZE
+                sliceStart += SLICE_SIZE
                 updateProgress(offset)
-                offset += line.__len__() #update offset before moving 
-        buf = diskImg.readlines(bufsize) #***make sure this line is indented to fall under the while loop, not under the inner for loop!
-outputFile.close()
-
-"""
-#debugging version
-for x in range(0, 8000): 
-        for line in buf:
-                offset_str = "Current offset: " + str(offset)
-                bytes_len = "Bytes (line length): " + str(line.__len__())
-                #output_1 = offset_str + "\n" + bytes_len + "\n" + str(line) + "\n" #raw interpretation is a mixture of bytes and ascii
-                output_2 = offset_str + "\n" + bytes_len + "\n" + line.hex("-") + "\n" #now its all as hex, as in active desk editor
-                #print(output_1)
-                #print(output_2)
-                updateProgress(offset)
-                #output_file.write(output + "\n")
-                offset += line.__len__() #update offset before moving 
-        buf = f.readlines(bufsize)
-output_file.close()
-"""
+        buf = DISKIMG.read(bufsize)
+OUTPUT_FILE.close()
