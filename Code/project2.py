b6dd98e190a1c109984df526b91f8bcc7b4048e0
amodkk <randomambipom@gmail.com>
2020-11-30 22:27:23 -0600

    MPG header matching. [Very unreliable MPG footer matching]. Moderate Refactoring.

--- Code/project2.py
+++ Code/project2.py
@@ -19,40 +19,53 @@ from array import *
 import re 
 from enum import Enum
 
-#"CONSTANTS" ----------------------------------------------------------
+
 #Paths
 CURRENTDIR = Path(__file__).parent #dunder/special variable __file__ 
 DISKIMG_FILENAME = "Project2Updated.dd"
 OUTPUT_FILENAME = "AnalysisOutput.txt"
-MATCHES_FILENAME = "MatchResults3.txt"
+MATCHES_FILENAME = "MatchResults.txt"
 DISKIMG_PATH = CURRENTDIR.joinpath(DISKIMG_FILENAME)
 DISKIMG_SIZE = Path(DISKIMG_PATH).stat().st_size #size of the disk image, in bytes 
 OUTPUT_PATH = CURRENTDIR.joinpath(OUTPUT_FILENAME) #for manual debugging 
 MATCHES_PATH = CURRENTDIR.joinpath(MATCHES_FILENAME) #for manual debugging
+
+
 #File Streams
 DISKIMG = open(DISKIMG_PATH, "rb")
 OUTPUT_FILE = open(OUTPUT_PATH, "w") #for manual debugging
-#Signatures: 2D-Array. Each element is a pair of strings of the format: [<Type of signature>, <Unique hexadecimal signature>]
-SIGS_ARR = [["AVI", "^52-49-46-46"], ["BMP", "^42-4D(?:-[0-9A-F]{2}){4}-00-00"], ["DOCX", "^50-4B-03-04-14-00-06-00"], 
-        ["GIF", "^47-49-46-38-39-61"], ["JPG", "^FF-D8-FF-E0"], ["PDF", "^25-50-44-46"], ["PNG", "^89-50-4E-47-0D-0A-1A-0A"]]
 
-TRAIL_ARR = [["AVI", "null"], ["BMP", "null"], ["DOCX", "50-4B-05-06"], 
-        ["GIF", "00-00-3B"], ["JPG", "FF-D9"], ["PDF", "25-25-45-4F-46"], ["PNG", "49-45-4E-44-AE-42-60-82"]]
 
+#2D-Array of files' unique header & footer sigs 
+#Each element is a set of 3 strings following the format: [<file type>, <Unique header sig>, <uniq footer sig>]
+HDR_FTR_ARR = [["AVI", "^52-49-46-46", "null"], 
+        ["BMP", "^42-4D(?:-[0-9A-F]{2}){4}-00-00", "null"], 
+        ["DOCX", "^50-4B-03-04-14-00-06-00", "50-4B-05-06"], 
+        ["GIF", "^47-49-46-38-39-61", "00-00-3B"], 
+        ["JPG", "^FF-D8-FF-E0", "FF-D9"], 
+        ["MPG", "^00-00-01-B(?:3|A)", "00-00-01-B[^3|A]{1}(?:-00)*$"], 
+        ["PDF", "^25-50-44-46", "25-25-45-4F-46"], 
+        ["PNG", "^89-50-4E-47-0D-0A-1A-0A", "49-45-4E-44-AE-42-60-82"]]
+
+
+#Identifies indices of the files in HDR_FTR_ARR
 class fileIndx(Enum): #enum values correspond to the file type's location in "SIGS_ARR"
         AVI = 0
         BMP = 1
         DOCX = 2
         GIF = 3
         JPG = 4
-        PDF = 5
-        PNG = 6
+        MPG = 5
+        PDF = 6
+        PNG = 7
+
 
 offset = 0 #current byte offset location in disk
 progress = 0 #progress made through disk analysis (in percent)
 bufsize = 4096 #to load larger amount of bytes at once into the program vs constsantly having to read from OS. speeds up program
 matchResults = "" 
-seekTrailer = -1
+seekFootSig = -1
+
 
 #Summary: takes in a hex string, slices it from 'start' to 'end', slice is interpreted as little-endian order. 
 # The slice is then converted to decimal format and returned. 
@@ -65,40 +78,41 @@ def littleEndianHexToDec(hexStr, start, end):
 
 
 #match signatures-- very premature version
-def matchSignatures(offset, data): 
+def matchHeaderSigs(offset, data): 
         global matchResults #indicating the global var will be changed in this scope
-        global seekTrailer  
+        global seekFootSig  
         i = 0
         regexMatch = []
-        while (i < len(SIGS_ARR)): #check for match against all signatures in SIGS_ARR
+        while (i < len(HDR_FTR_ARR)): #check for match against all signatures in SIGS_ARR
                 fileType = fileIndx(i).name
-                fileSig = SIGS_ARR[i][1] #BMP requires regex matching
+                fileSig = HDR_FTR_ARR[i][1] 
                 regexMatch = re.findall(fileSig, data) #param 1 contains the regex expression, checks against param2
                 if(regexMatch): #if any potential signature found
                         result = fileType + " signature offset: " + str(offset) #str(match + offset)
                         print(result)
                         matchResults += result + "\n"
-                        if(i in (fileIndx.AVI.value, fileIndx.BMP.value, fileIndx.DOCX.value, fileIndx.GIF.value, 
-                        fileIndx.JPG.value, fileIndx.PDF.value, fileIndx.PNG.value)): #adding one as a time as debugging
-                                seekTrailer = i
+                        if(i != fileIndx.MPG.value): #seek footer sig for everything beside MPG, MPG needs perfecting, see trello
+                                seekFootSig = i
                         else: 
-                                seekTrailer = -1 #only debugging for jpg at this point
+                                seekFootSig = -1 #MPG needs perfecting
                         return 
                 i += 1
 
+
 #match trailer for the associated signature
-def matchTrailer(offset, data): 
+#result is a string that will be printed to terminal for debugging
+def matchFooterSigs(offset, data): 
         global matchResults #indicating the global var will be changed in this scope
-        global seekTrailer
+        global seekFootSig
         endSeek = False
         offsetAdjust = 0
-        fileType = fileIndx(seekTrailer).name
-        fileTrail = TRAIL_ARR[seekTrailer][1]
-        if(seekTrailer == -1): 
+        fileType = fileIndx(seekFootSig).name
+        footerSig = HDR_FTR_ARR[seekFootSig][2]
+        if(seekFootSig == -1): 
                 return
         #AVI, BMP
-        if(seekTrailer in (fileIndx.AVI.value, fileIndx.BMP.value)):  
-                if(seekTrailer == fileIndx.AVI.value): 
+        if(seekFootSig in (fileIndx.AVI.value, fileIndx.BMP.value)):  
+                if(seekFootSig == fileIndx.AVI.value): 
                         decSize = littleEndianHexToDec(data, 4, 8)
                         offsetAdjust = decSize + 7 #for AVI, data size indicates bytes AFTER the first 7 bytes of header
                 else: #BMP  
@@ -107,24 +121,27 @@ def matchTrailer(offset, data):
                 result = fileType + " ending offset: " + str(offset + offsetAdjust) + " (inclusive)"
                 endSeek = True
 
-        #DOCX, GIF, JPG, PDF, PNG 
-        elif(seekTrailer in (fileIndx.DOCX.value, fileIndx.GIF.value, fileIndx.JPG.value, fileIndx.PDF.value, fileIndx.PNG.value)):
-                regexMatch = re.findall(fileTrail, data)
+        #Everything else: DOCX, GIF, JPG, MPG, PDF, PNG, MPG
+        else:
+                regexMatch = re.findall(footerSig, data)
+                if(seekFootSig == fileIndx.MPG.value): 
+                        footerSig = regexMatch[0] #yea this doesn't work as is lol.. too many false positives
                 if(regexMatch):
                         #adjusting offset
                         footerLineBytes = bytes.fromhex(data.replace("-", " "))
-                        trailBytes = bytes.fromhex(fileTrail.replace("-", " "))
-                        preTrailerSize = footerLineBytes.find(trailBytes)
-                        trailerSize = len(trailBytes)
-                        offsetAdjust = preTrailerSize + trailerSize - 1
-                        if(seekTrailer == fileIndx.DOCX.value): 
+                        footerSigBytes = bytes.fromhex(footerSig.replace("-", " "))
+                        preFSigBytes = footerLineBytes.find(footerSigBytes) #number of bytes before footer sig
+                        fSigBytes = len(footerSigBytes) #number of bytes of footer sig
+                        offsetAdjust = preFSigBytes + fSigBytes - 1
+                        if(seekFootSig == fileIndx.DOCX.value): 
                                 offsetAdjust += 18 #DOCX files have an additional 18 bytes after the trailer sequence
                         result = fileType + " ending offset: " + str(offset + offsetAdjust) + " (inclusive)"
                         endSeek = True
         if(endSeek): 
                 print(result)
                 matchResults += result + "\n\n"
-                seekTrailer = -1
+                seekFootSig = -1
+
 
 #update disk analysis every progress every 5%
 #gets current progress percentage by dividing: (current byte offset/disk image byte size)
@@ -137,9 +154,12 @@ def updateProgress(offset):
                 if(progress == 100): 
                         print("-- Analysis completed --")
 
+
 #DEBUG LINES
-#DISKIMG.seek(47820800) #debugging BMP regex quicker by starting from that offset
-#offset = 47820800
+#DISKIMG.seek(47960064) #debugging BMP regex quicker by starting from that offset
+#offset = 47960064
+
+
 #this loop reads the diskimage and prints it in hex format to output file
 buf = DISKIMG.read(bufsize)
 while buf: #continues looping as long as 'buf' variable is populated
@@ -150,9 +170,9 @@ while buf: #continues looping as long as 'buf' variable is populated
         #this loop will analyze the buffer in 16-byte segments.
         while (sliceStart < buffLength): 
                 line = buf[sliceStart:sliceStart + SLICE_SIZE].hex('-').upper() #16-byte line of the diskimage data in hex format
-                matchSignatures(offset, line)
-                if(seekTrailer != -1): 
-                        matchTrailer(offset, line)
+                matchHeaderSigs(offset, line)
+                if(seekFootSig != -1): 
+                        matchFooterSigs(offset, line)
                 OUTPUT_FILE.write("Offset " + str(offset) + " | " + line  + "\n")
                 offset += SLICE_SIZE
                 sliceStart += SLICE_SIZE
@@ -166,3 +186,4 @@ MATCHES_FILE = open(MATCHES_PATH, "w")
 MATCHES_FILE.write("-- MATCH RESULTS --\n")
 MATCHES_FILE.write(matchResults)
 MATCHES_FILE.close()
+print("-- See output file 'MatchResults' for manual debugging --")
